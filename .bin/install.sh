09917125f08cfe9ea563dfffb1d3b23714af4a0f
Takeshi KATO <kato@cuebs.co.jp>
2024-12-11 23:16:24 +0900

    dotfileの追加

--- .bin/install.sh
+++ .bin/install.sh
@@ -16,15 +16,16 @@ link_to_homedir() {
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
   local dotdir=$(dirname ${script_dir})
   if [[ "$HOME" != "$dotdir" ]]; then
-    for f in $dotdir/.??**; do 
-      [[ `basename $f` == ".git" ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]]; then 
-        command rm -f "$HOME/`basename $f`"
+    for f in $dotdir/*; do 
+      fname="$(basename $f)"
+      echo $fname
+      if [[ -L "$HOME/.$fname" ]]; then 
+        command rm -f "$HOME/.`basename $f`"
       fi
-      if [[ -e "$HOME/`basename $f`" ]]; then 
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+      if [[ -e "$HOME/.$fname" ]]; then 
+        command mv "$HOME/.$fname" "$HOME/.dotbackup"
       fi
-      command ln -snf $f $HOME
+      command ln -snf $f $HOME/.$fname
     done
   else
     command echo "same install src dest"
